4f9ecb47662c4a186cea8c35656eee77f71193d8
Alisha P <apegan@wellesley.edu>
2017-03-10 01:16:35 -0500

    IT WORKS

--- timeline_plot.py
+++ timeline_plot.py
@@ -5,11 +5,16 @@ import collections
 from collections import OrderedDict
 from collections import defaultdict
 from operator import itemgetter
+import datetime
 from datetime import date
 import plotly
+import plotly.offline as py
+import cufflinks as cf
 
-import plotly.offline as offline
 import plotly.graph_objs as go
+
+cf.go_offline()
+py.init_notebook_mode()
 
 with open('michelle_locations.json', 'r') as json_data:
     mdata = json.load(json_data)
@@ -22,25 +27,36 @@ with open('hillary_locations.json', 'r') as json_data:
 shortdate = []
 distance = []
 description = []
+location = []
+title = []
 for phrase in mdata:
     for article in phrase:
         shortdate.append(article['shortdate'])
         distance.append(article['distance'])
+        location.append(article['location'])
         description.append(article['description'])
+        title.append(article['title'])
 
-shortdate = [i for i, data in enumerate(shortdate)]
+# shortdate = [i for i, data in enumerate(shortdate)]
+use_date = [d.date() for d in pd.to_datetime(shortdate)]
 
-print(shortdate)
-print(distance)
+m_df = pd.DataFrame(
+    {'Time': use_date,
+     'Location': location,
+     'Miles from D.C.': distance,
+     'Description': description,
+     'Title': title})
 
-offline.init_notebook_mode()
-# offline.plot({'data': [{'y': [4, 2, 3, 4]}],
-#                'layout': {'title': 'Test Plot',
-#                           'font': dict(size=16)}},
-#              image='png')
+# print(m_df)
 
-# mtrace = go.Scatter(x=shortdate, y=distance)
-offline.plot({'data': [{'x': shortdate, 'y': distance}]}, image='png')
+mm = m_df.sort_values(['Time'], ascending=True)
+
+print(mm['Time'])
+
+# mm.iplot(kind='scatter', title='trying')
+
+# mm.offline.plot(kind='scatter', filename='trying')
+py.plot({'data': [{'x': mm['Time'], 'y': mm['Miles from D.C.'], 'text': mm['Title']}]}, filename='lines.html')
 # for phrase in mdata:
 #         sorted_x = OrderedDict(sorted(phrase.items(), key=itemgetter(0)))
 #         print(sorted_x)
